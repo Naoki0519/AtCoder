259aad35c1afdf6d50c6d258683f330b8966730e
3870 <3810naoki@gmail.com>
2026-06-06 09:15:33 +0900

    #1

---
+++ 競プロ典型90問/1.py
@@ -0,0 +1,15 @@
+## https://atcoder.jp/contests/typical90/tasks/typical90_a
+import sys
+input = sys.stdin.readline
+
+
+def main():
+    # 入力
+    N, L = map(int, input().split())
+    K = int(input())
+    A = list(map(int, input().split()))
+    
+    
+
+if __name__ == "__main__":
+    main()

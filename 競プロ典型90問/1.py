f7f4e1f38a1581e2a1a90444ec4ba6689dbc71fe
3870 <3810naoki@gmail.com>
2026-06-08 11:05:22 +0900

    feat/1.py AC

--- 競プロ典型90問/1.py
+++ 競プロ典型90問/1.py
@@ -1,15 +1,38 @@
-## https://atcoder.jp/contests/typical90/tasks/typical90_a
-import sys
-input = sys.stdin.readline
+# https://atcoder.jp/contests/typical90/tasks/typical90_a
 
+# 入力を受けとる
+n, l = map(int, input().split())# 切れ目の数，羊羹の長さ
+k = int(input())# 選ぶ切れ目の数
+a = list(map(int, input().split()))# 切れ目の位置
 
-def main():
-    # 入力
-    N, L = map(int, input().split())
-    K = int(input())
-    A = list(map(int, input().split()))
-    
-    
+a.append(l)# 羊羹の右端も切れ目の位置に追加する
 
-if __name__ == "__main__":
-    main()
+# 切り込み間の長さをdifferenceに保持
+difference = [a[0]]
+for i in range(n):
+    difference.append(a[i+1]-a[i])
+
+# 二分探索によりスコアが最大となるものを探索
+def binary_search(left, right):# 範囲の最小値，最大値を受け取り，スコアを返す
+    mid = (left + right) //2
+    if (mid == left):
+        return left
+    if is_ok(mid):
+        return binary_search(mid, right)
+    else:
+        return binary_search(left, mid)
+
+def is_ok(mid):# そのスコアが条件を満たすかチェックし満たす場合はその時のscoreを返す
+    count = 0
+    current_length = 0
+    for length in difference:
+        current_length = current_length + length
+        if current_length >= mid:# 合計が現在テストしている長さ以上になった時点でcountを上げて今までの長さの合計をリセットする
+            count = count + 1
+            current_length = 0
+    if count >= k+1:
+        return True
+    else:
+        return False
+
+print(binary_search(0,l))

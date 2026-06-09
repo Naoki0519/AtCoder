663ef0ee5935badf205fb5ce8949836ae22e536b
3870 <3810naoki@gmail.com>
2026-06-09 23:51:10 +0900

    feat: #2

---
+++ 競プロ典型90問/2.py
@@ -0,0 +1,18 @@
+# https://atcoder.jp/contests/typical90/tasks/typical90_b
+
+
+S = int(input())
+if S % 2 == 1: #奇数なら空文字を出力
+        print("")
+else:
+      for i in range(2^S):
+             
+             
+
+
+
+
+
+
+
+

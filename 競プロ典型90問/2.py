f9d56f9188144137f1b0f6d1a98ec4d72aaab207
3870 <3810naoki@gmail.com>
2026-06-11 16:30:55 +0900

    fix: #2 実行時間の短縮

--- 競プロ典型90問/2.py
+++ 競プロ典型90問/2.py
@@ -6,6 +6,8 @@ if N % 2 == 1: #奇数なら何も出力しない
         pass
 else:
       for i in range(2**N): #全ビット探索を行う
+             if bin(i).count('1') != N//2: #0の数と1の数が半々である必要がある．
+                    continue
              s = []
              for j in range(N): #iの各ビットについて上位bitから調べる
                    bit = ((i >> (N - j - 1)) & 1)
@@ -13,16 +15,15 @@ else:
                            s.append("(")
                    elif bit == 1:
                            s.append(")")
-             if s.count("(") == s.count(")"): # (と)の数が同じ
-                    is_valid = True #正しい()か
-                    depth = 0 #（の数が）の数より現状何個多く出現しているか
-                    for t in s:
-                           if t == "(":
-                                  depth = depth + 1
-                           elif t == ")":
-                                  depth = depth - 1
-                           if depth < 0:
-                                  is_valid = False
-                                  break
-                    if is_valid:
-                           print("".join(s))
+             is_valid = True #正しい()か
+             depth = 0 #（の数が）の数より現状何個多く出現しているか
+             for t in s:
+                   if t == "(":
+                           depth = depth + 1
+                   elif t == ")":
+                           depth = depth - 1
+                   if depth < 0:
+                           is_valid = False
+                           break
+             if is_valid:
+                print("".join(s))
